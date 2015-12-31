21a80b834eb6173de904e01bd6a91b4af24ea1ce
Jess Portnoy <jess.portnoy@kaltura.com>
2015-12-31 15:38:16 +0000

    double define removed.

--- php_jam.h
+++ php_jam.h
@@ -38,7 +38,6 @@
 #include <sys/resource.h>
 
 #define PHP_5_3_X_API_NO                20090626
-#define PHP_5_3_X_API_NO                20090626
 #define PHP_5_4_X_API_NO                20100525
 #define PHP_5_5_X_API_NO                20121212
 #define PHP_5_6_X_API_NO                20131226
